312bc723e85b565a70b8549dd56204a0c3598e3b
Tom Zheng <t5zheng@ucsd.edu>
2025-03-14 01:42:59 -0700

    google stuff

--- linkd-nextjs/app/layout.tsx
+++ linkd-nextjs/app/layout.tsx
@@ -52,6 +52,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RSW8XT7RP8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RSW8XT7RP8');
+          `}
+        </Script>
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#ffffff" />
         
